6ef3f64f144b45cfae726cf846762e523487c962
Rahul Ranjan <RAHUL1.777402@mmamc.tu.edu.np>
2025-05-04 15:52:23 +0545

    index update

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo '<h1>hello its a collab!</h1>';
-echo'<img src="mnr.jpg"width="700">';
+echo '<img src="mnr.jpg" width="700">';
+echo '<br><a href="newfie.php">This is a new page</a>';
 ?>
